318d84d3d1280376989baa567fdb4bac28e6a99a
Jacky11221 <144474368+Jacky11221@users.noreply.github.com>
2024-12-12 13:19:48 -0500

    Add files via upload

--- src/Day8/Day8.java
+++ src/Day8/Day8.java
@@ -1,5 +1,6 @@
 package Day8;
 
+import java.awt.image.PackedColorModel;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -15,9 +16,50 @@ public class Day8 {
         System.out.println(Arrays.deepToString(fileData2D));
         System.out.println(symbols);
 
+//        for(String ssymop: symbols){
+//            System.out.println(getCoordsForPoints(ssymop,fileData2D));
+//        }
+
+        ArrayList<String> aCoords = getCoordsForPoints(symbols.get(1),fileData2D);
 
 
+
+        System.out.println(aCoords);
+        for(int i = 0; i < aCoords.size(); i++){
+            String[] xy1 = splitToArray(aCoords.get(i),",");
+            for(int j = i+1; j < aCoords.size(); j++){
+                String[] xy2 = splitToArray(aCoords.get(j),",");
+
+
+                System.out.println("xy1: " + Arrays.toString(xy1));
+                System.out.println("xy2: " + Arrays.toString(xy2));
+
+                int xDiff = Integer.parseInt(xy2[0]) - Integer.parseInt(xy1[0]);
+
+            }
+        }
+//        for(int i = 0; i < aCoords.size()-1; i++){
+//            String[] firstxy = splitToArray(aCoords.get(i),",");
+//            String[] secondxy = splitToArray(aCoords.get(i+1),",");
+//            System.out.println(
+//                    (Integer.parseInt(secondxy[1])-Integer.parseInt(firstxy[1]))
+//                            /(Integer.parseInt(secondxy[0])-Integer.parseInt(firstxy[0]))
+//            );
+//        }
     }
+
+
+//    public static ArrayList<String> getSlope(ArrayList<String> coords){
+//        for(int i = 0; i < coords.size()-1; i++){
+//            String[] firstxy = splitToArray(coords.get(i),",");
+//            String[] secondxy = splitToArray(coords.get(i+1),",");
+//            System.out.println(
+//                    (Integer.parseInt(secondxy[1])-Integer.parseInt(firstxy[1]))
+//                            /(Integer.parseInt(secondxy[0])-Integer.parseInt(firstxy[0]))
+//            );
+//        }
+//        return nothing;
+//    }
 
     public static ArrayList<String> getUniqueSymbols(String[][] fileData2D){;
         ArrayList<String> symbols = new ArrayList<String>();
@@ -40,6 +82,17 @@ public class Day8 {
             }
         }
         return symbols;
+    }
+
+    public static ArrayList<String> getCoordsForPoints(String symbol,String[][] grid){
+        ArrayList<String> coords = new ArrayList<>();
+
+        for (int r = 0; r < grid.length; r++) {
+            for (int c = 0; c < grid[0].length; c++) {
+                if (Objects.equals(grid[r][c], symbol)) coords.add(r + "," + c);
+            }
+        }
+        return coords;
     }
 
     public static String[][] getFileDataAs2DArray(ArrayList<String> fileData){
@@ -75,5 +128,9 @@ public class Day8 {
     public static String[] splitToArray(String str){
         return str.split(" ");
     }
-}
 
+    public static String[] splitToArray(String str, String regex){
+        return str.split(regex);
+    }
+
+}
