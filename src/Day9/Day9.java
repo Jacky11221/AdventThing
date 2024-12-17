818cd51736cf77601c5efb7fc964bf1dc93fdd30
Jacky11221 <144474368+Jacky11221@users.noreply.github.com>
2024-12-16 21:02:24 -0500

    tired

--- src/Day9/Day9.java
+++ src/Day9/Day9.java
@@ -3,47 +3,87 @@ package Day9;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Day9 {
     public static void main(String[] args) {
-        ArrayList<String> fileData = getFileData("src/Day9/day9Input.txt");
-        System.out.println(fileData);
-        System.out.println(quantizedData(fileData));
+        ArrayList<String> fileData1 = getFileData("src/Day9/day9Input.txt");
+        ArrayList<String> fileData2 = getFileData("src/Day9/day9Input.txt");
+
+//        ArrayList<String> quantizedData1 = quantizeData(fileData1);
+//        System.out.println("quantized1:" + quantizedData1);
+//        ArrayList<String> astralQuantification = replaceBlanks(quantizedData1);
+//        System.out.println("Part 1 ans:" + checkSum(astralQuantification));
+
+
+        ArrayList<String> quantizedData2 = quantizeData(fileData2);
+        System.out.println("quantized2:" + quantizedData2);
+        ArrayList<String> designatedQuantification = designateData(quantizedData2);
+        System.out.println("Part 2");
 
     }
 
-    public static String replaceBlanks(ArrayList<String> data){
+    private static ArrayList<String> designateData(ArrayList<String> data) {
         for(int i = 0; i < data.size(); i++){
-            if(data.get(i).equals(".")){
-                //do stuff
+            if(Objects.equals(data.get(i), ".")){
+                int blankDataLength = 0;
+                int j = i;
+                while (Objects.equals(data.get(j), ".")){
+                    blankDataLength++;
+                    j++;
+                }
+                i = j;
+                System.out.println(blankDataLength);
+                //add things here maybe
+
             }
         }
-        return "stuff";
+        return data;
     }
 
-    public static String quantizedData(ArrayList<String> fileData){
+    public static long checkSum(ArrayList<String> data){
+        long sum = 0;
+        for(int i = 0; i < data.size(); i++){
+            sum += (long) Integer.parseInt(data.get(i)) *i;
+        }
+        return sum;
+    }
+
+    public static ArrayList<String> replaceBlanks(ArrayList<String> data){
+        for(int i = 0; i < data.size(); i++){
+            if(data.get(i).equals(".")){
+                if(Objects.equals(data.getLast(), ".")) {
+                    data.removeLast();
+                    i--;
+                }
+                else {
+                    data.add(i, data.getLast());
+                    data.remove(i + 1);
+                    data.removeLast();
+                }
+            }
+        }
+        return data;
+    }
+
+    public static ArrayList<String> quantizeData(ArrayList<String> fileData){
         String data = fileData.getFirst();
-        String quantized = "";
+        ArrayList<String> quantized = new ArrayList<String>();
         for(int i = 0; i < data.length(); i++){
             int currentNumber = Integer.parseInt(data.substring(i,i+1));
             if(i%2 == 0){
-                quantized += makeString(currentNumber, String.valueOf(i/2));
+                for(int j = 0; j < currentNumber; j++){
+                    quantized.add(String.valueOf(i/2));
+                }
             }
             else{
-                quantized += (makeString(currentNumber, "."));
+                for(int j = 0; j < currentNumber; j++){
+                    quantized.add(".");
+                }
             }
         }
         return quantized;
-    }
-
-    public static String makeString(int length, String str){
-        String string = "";
-        for(int i = 0; i < length; i++){
-            string += str;
-        }
-        return string;
     }
 
 
